752fc67256f7ddcd80e68e7b1522d37790ebb79c
Mulugeta Hailegnaw <mulugetahail89@gmail.com>
2023-01-09 10:54:09 +0300

    using global dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,13 +9,8 @@ export class authController{
     constructor(private authservice: AuthService) {
     }
     @Post('signup')
-    signup(@Body("email") email: string, @Body("password", ParseIntPipe) password: string) {
-        console.log({
-            email,
-            typeOfEmail: typeof email,
-            password,
-            typeOfPassword: typeof password
-        })
+    signup(@Body() dto: authDto) {
+        console.log(dto.email, dto.password)
         return this.authservice.signup()
     }
     @Post("login")
--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -1,4 +1,12 @@
-export interface authDto{
+import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+//import { IsNotEmpty, IsString } from "class-validator/types/decorator/decorators"
+
+export class authDto{
+    @IsEmail()
+    @IsNotEmpty()
     email: String
+
+    @IsString()
+    @IsNotEmpty()
     password: String
 }
